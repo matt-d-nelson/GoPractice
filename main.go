ddd66842ecb59cae5ec11b39992fe6a766aea3b6
Matt Nelson <mattnelson6992@gmail.com>
2022-04-19 09:46:06 -0500

    CodeAcademy: comic mischief

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 
+	comicMischief "github.com/matt-d-nelson/GoPractice/codeAcademy"
 	"github.com/matt-d-nelson/GoPractice/practice"
 )
 
 func main() {
 	practice.HelloWorld()
 	fmt.Println(practice.Solution("mynameismatt"))
-
+	comicMischief.PrintComics()
 }
